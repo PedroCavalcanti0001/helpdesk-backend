b4ccad0942022027ff06eb2a479f6eaf2378051b
pedro <pedroeugeniocavalcanti@gmail.com>
2022-07-15 18:30:05 -0300

    Novo tecnico para testes adicionado.

--- src/main/java/com/pedroeugenio/helpdesk/services/DBService.java
+++ src/main/java/com/pedroeugenio/helpdesk/services/DBService.java
@@ -34,6 +34,9 @@ public class DBService {
         Tecnico tec2 = new Tecnico(null, "Leozinho Vitorio", "188.122.630-11", "leozinhovito@gmail.com","12345");
         tec1.addPerfil(Perfil.ADMIN);
 
+        Tecnico tec3 = new Tecnico(null, "André", "858.393.700-10",  "andre@gmail.com","12345");
+        tec1.addPerfil(Perfil.ADMIN);
+
 
         Cliente cli1 = new Cliente(null, "Linus Torvalds", "824.518.180-34", "linustorvalds@hotmail.com","12345");
         Chamado ch1 = new Chamado(null, Prioridade.ALTA, Status.ANDAMENTO, "Chamado 01", "Primeiro Chamado", tec1, cli1);
@@ -42,7 +45,7 @@ public class DBService {
         Cliente cli2 = new Cliente(null, "Steve Jobs", "490.227.830-81", "stevejobs@protonmail.com", "12345");
         Chamado ch2 = new Chamado(null, Prioridade.MEDIA, Status.ABERTO, "Chamado 02", "Segundo Chamado", tec2, cli2);
 
-        tecnicoRepository.saveAll(Arrays.asList(tec1, tec2));
+        tecnicoRepository.saveAll(Arrays.asList(tec1, tec2, tec3));
         clienteRepository.saveAll(Arrays.asList(cli1, cli2));
         chamadoRepository.saveAll(Arrays.asList(ch1, ch2));
     }
