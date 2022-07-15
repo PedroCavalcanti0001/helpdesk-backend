6f8de798a9bb0568174f87fb192866941e6f36eb
pedro <pedroeugeniocavalcanti@gmail.com>
2022-07-15 17:48:35 -0300

    Validacao de CPF

--- src/main/java/com/pedroeugenio/helpdesk/domain/Pessoa.java
+++ src/main/java/com/pedroeugenio/helpdesk/domain/Pessoa.java
@@ -2,6 +2,7 @@ package com.pedroeugenio.helpdesk.domain;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.pedroeugenio.helpdesk.domain.enums.Perfil;
+import org.hibernate.validator.constraints.br.CPF;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -20,6 +21,7 @@ public abstract class Pessoa implements Serializable {
     protected Integer id;
     protected String nome;
     @Column(unique = true)
+    @CPF
     protected String cpf;
     @Column(unique = true)
     protected String email;
--- src/main/java/com/pedroeugenio/helpdesk/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/pedroeugenio/helpdesk/resources/exceptions/ResourceExceptionHandler.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
@@ -43,14 +45,26 @@ public class ResourceExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<StandardError> validationErrors(MethodArgumentNotValidException ex,
                                                           HttpServletRequest request) {
-
+            ValidationError errors = new ValidationError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(),
+                    "Validation error",
+                    "Erro na validação dos campos",
+                    request.getRequestURI()
+            );
+            for (FieldError x : ex.getBindingResult().getFieldErrors()) {
+                errors.addError(x.getField(), x.getDefaultMessage());
+            }
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    }
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<StandardError> validateCpf(ConstraintViolationException ex,
+                                                          HttpServletRequest request) {
         ValidationError errors = new ValidationError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(),
                 "Validation error",
                 "Erro na validação dos campos",
                 request.getRequestURI()
         );
-        for (FieldError x : ex.getBindingResult().getFieldErrors()) {
-            errors.addError(x.getField(), x.getDefaultMessage());
+        for (ConstraintViolation<?> x : ex.getConstraintViolations()) {
+            errors.addError(x.getPropertyPath().toString(), x.getMessage());
         }
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
     }
