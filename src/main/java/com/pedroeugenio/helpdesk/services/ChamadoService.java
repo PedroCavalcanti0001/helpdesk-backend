484c262d996169e867950203bf158486f918139f
pedro <pedroeugeniocavalcanti@gmail.com>
2022-07-19 16:17:21 -0300

    findById para chamados

---
+++ src/main/java/com/pedroeugenio/helpdesk/services/ChamadoService.java
@@ -0,0 +1,21 @@
+package com.pedroeugenio.helpdesk.services;
+
+import com.pedroeugenio.helpdesk.domain.Chamado;
+import com.pedroeugenio.helpdesk.repositories.ChamadoRepository;
+import com.pedroeugenio.helpdesk.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ChamadoService {
+
+    @Autowired
+    private ChamadoRepository repository;
+
+    public Chamado findById(Integer id){
+        Optional<Chamado> obj = repository.findById(id);
+         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! id: "+id));
+    }
+}
